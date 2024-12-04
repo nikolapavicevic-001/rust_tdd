88b2c16a033a910495d2ab4280140731396319b4
Nikola Pavicevic <nikolapavicevic95@gmail.com>
2024-12-04 16:49:35 +0100

    Write a failing test for celsius to kelvin.

--- Readme.md
+++ Readme.md
@@ -3,3 +3,6 @@ Unit tests should be in same file as function.\
 Integration test are located in tests folder.
 # CI
 CI will run when pull request has been made to development branch
+# Tests
+I am not sure if it is good practice to have multiple asserts in same case.
+
---
+++ src/temperature_utils.rs
@@ -0,0 +1,15 @@
+fn celsius_to_kelvin(celsius : f32) -> f32 {
+    return 0.0;
+}
+
+#[cfg(test)]
+mod tests{
+
+    use super::*;
+    #[test]
+    fn test_celsius_to_kelvin(){
+        assert_eq!(celsius_to_kelvin(0.0), 273.15);
+        assert_eq!(celsius_to_kelvin(3.0), 276.15);
+        assert_eq!(celsius_to_kelvin(-30.0), 243.15);
+    }
+}
