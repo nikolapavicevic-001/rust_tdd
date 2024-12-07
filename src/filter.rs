0d417ace62b16401a5c786d060033284c968220f
Nikola Pavicevic <nikolapavicevic95@gmail.com>
2024-12-07 22:15:57 +0100

    Implemented filter data method

--- src/filter.rs
+++ src/filter.rs
@@ -1,16 +1,17 @@
-struct Filter<const N: usize>{
-    buffer: [u8; N]
+use std::usize;
+
+pub struct Filter<const N: usize> {
+    buffer: [u32; N],
 }
 
-impl<const N: usize> Filter<N>{
+impl<const N: usize> Filter<N> {
     pub fn new() -> Self {
-        Filter{
-            buffer: [0; N]
-        }
+        Filter { buffer: [0; N] }
     }
 
-    pub fn filter_data(&self) -> u8 {
-        return 0;
+    pub fn filter_data(&self) -> u32 {
+        let sum: u32 = self.buffer.iter().map(|&x| x).sum();
+        sum / self.buffer.len() as u32
     }
 }
 
@@ -21,20 +22,25 @@ mod tests {
     use super::*;
 
     #[fixture]
-    fn filter() -> Filter<10>{
+    fn filter() -> Filter<10> {
         let filter = Filter::<10>::new();
         filter
     }
 
+    fn set_buffer_value(filter: &mut Filter<10>, value: u32) {
+        filter.buffer = filter.buffer.map(|x| value);
+    }
+
     #[rstest]
-    fn test_empty_buffer(filter: Filter<10>){
-        for i in filter.buffer{
+    fn test_empty_buffer(filter: Filter<10>) {
+        for i in filter.buffer {
             assert_eq!(i, 0)
         }
     }
 
     #[rstest]
-    fn test_filter_data(filter: Filter<10>){
+    fn test_filter_data(mut filter: Filter<10>) {
+        set_buffer_value(&mut filter, 1);
         assert_eq!(1, filter.filter_data())
     }
 }
