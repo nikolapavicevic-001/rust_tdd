df8862c36fa8939d2c40680c32946b2b776d83b5
Nikola Pavicevic <nikolapavicevic95@gmail.com>
2024-12-10 23:57:51 +0100

    Finished exercise_3

--- src/thermal_reader.rs
+++ src/thermal_reader.rs
@@ -1,14 +1,12 @@
-use crate::{config::BUFFER_SIZE, filter::Filter};
+use crate::filter::FilterMethods;
 
 struct ThermalReader {
-    filter: Filter<BUFFER_SIZE>,
+    filter: Box<dyn FilterMethods>,
 }
 
-impl ThermalReader{
-    pub fn new(filter: Filter<BUFFER_SIZE>) -> Self{
-        Self{
-            filter
-        }
+impl ThermalReader {
+    pub fn new(filter: Box<dyn FilterMethods>) -> Self {
+        Self { filter }
     }
     pub fn read_filtered_temperature(&self) -> u32 {
         self.filter.filter_data()
@@ -16,12 +14,30 @@ impl ThermalReader{
 }
 
 #[cfg(test)]
-mod test{
-    use rstest::fixture;
+mod test {
+    use rstest::{fixture, rstest};
+
+    use crate::filter::MockFilterMethods;
 
     use super::*;
 
+    // mock! {
+    //     pub Filter{ }
+    //     impl FilterMethods for Filter{
+    //         fn filter_data(&self) -> u32;
+    //     }
+    // }
+
     #[fixture]
-    fn thermal_reader() -> ThermalReader{
+    fn thermal_reader() -> ThermalReader {
+        let mut mock_filter = MockFilterMethods::new();
+        mock_filter.expect_filter_data().return_const(40 as u32);
+
+        ThermalReader::new(Box::new(mock_filter))
+    }
+
+    #[rstest]
+    fn test_thermal_reader(thermal_reader: ThermalReader) {
+        assert_eq!(thermal_reader.read_filtered_temperature(), 40);
     }
 }
